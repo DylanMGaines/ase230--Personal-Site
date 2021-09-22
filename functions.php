b51e3bfc151586fe1c056c266b118221ca68a25d
Dylan <dylanmgaines1@gmail.com>
2021-09-22 15:55:47 -0400

    Commentless Completion
    Final version for Assignment 3.
    Includes Age calc function, data.php is now a data file, functions.php is now a swiss army knife of functions, some commented some not. Front end pages only had minor cosmetic changes

---
+++ functions.php
@@ -0,0 +1,118 @@
+<?php
+/** INFO cardLoader
+ * Takes a reference to an associative array as parameter.
+ * Main cardloader function, called from within index.php.
+ * Moved to its own function to make index.php cleaner. Used to be loop in index.php.
+ * Loops through array and sends each student entry through the helper function for display.
+ **/
+function cardLoader(&$studentArray) {
+    $len = count($studentArray);                                    //determine length
+    $keyRing = array_keys($studentArray);                           //determine keys for input array
+    for ($i = 0; $i < $len; $i++) {
+        $key = $keyRing[$i];                                        //get key of current entry
+        cardLoaderHelper($studentArray[$key], $key);        //send current entry to helper
+    }
+}
+
+/**INFO cardLoaderHelper
+ *  Takes a reference to an associative array and the key of said array.
+ *  Echoes a card for the student. Card links to detail.php with $key as the query variable "id". Also links to socials.
+ **/
+function cardLoaderHelper(&$student, $key) {
+    echo '<div class="col-12 col-sm-6 col-lg-3">
+                        <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                                <div class="advisor_thumb">
+                                    <!--anchor with link to detail, image b/t anchor tags-->
+                                    <a href="detail.php?id=', $key, '"><img src="', $student["img"], '" alt="" style =
+                                        "width: 315px; height: 315px;"></a>
+                                    <div class="social-info">
+                                        <a href="', $student["facebook"], '">
+                                            <i class="fa fa-facebook"></i>
+                                        </a>
+                                        <a href="', $student["twitter"], '">
+                                            <i class="fa fa-twitter"></i>
+                                        </a>
+                                        <a href="', $student["linkedIn"], '">
+                                            <i class="fa fa-linkedin"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                                <!-- Team Details-->
+                                <div class="single_advisor_details_info">
+                                    <h6>', $student["name"], '</h6>
+                                    <p class="designation">', $student["job"], '</p>
+                                    <div>';
+    for ($i = 0; $i < $student["year"]; $i++) {
+        echo '<span class="h5 bi-slash-lg"></span>';                //loop displays a slash for each year
+    }
+    echo "<ul>";
+    chronos($student["DOB"]);
+    echo "</ul>";
+    echo '                          </div>
+                                </div>
+                        </div>
+          </div>';
+}
+
+function skillBar(&$skills) {
+    $len = count($skills);
+    for ($i = 0; $i < $len; $i++) {
+        $skill = $skills[$i];
+        echo '<div class="py-1">
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width:', $skill["value"], '%;" aria-valuenow="',
+        $skill["value"], '" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar-title">', $skill["name"], '</div>
+                        <span class="progress-bar-number">', $skill["value"], '%</span>
+                    </div>
+                </div>
+              </div>';
+    }
+}
+
+function chronos($DoB) {
+    $bDay = date_create($DoB);
+    $jetzt = date_create();
+
+    $time = date_diff($jetzt, $bDay);
+
+    for ($i = 0; $i <= 3; $i++) {
+        ananke($i, $time);
+    }
+}
+
+function ananke($thing, $time) {
+    $theLad = "";
+    $theMad = "";
+    switch ($thing) {
+        case 3:
+        {
+            $theLad = "%m";
+            $theMad = "months";
+            break;
+        }
+        case 2:
+        {
+            $theLad = "%d";
+            $theMad = "days";
+            break;
+        }
+        case 1:
+        {
+            $theLad = "%y";
+            $theMad = "years";
+            break;
+        }
+        default :
+        {
+            $theLad = "%y";
+            $theMad = "age";
+        }
+    }
+    echo "<li class='media p-0 mb-0'>
+                        <span class='w-25 text-black font-weight-normal'>" . $theMad . "</span>
+                        <label class='media-body p-0 mb-0'>";
+    echo $time->format($theLad);
+    echo "               </label>
+          </li>";
+}
--- index.php
+++ index.php
@@ -5,58 +5,8 @@
     <?php
     //At some point, I'd like to load these cards dynamically, but for now, I will leave it. I'm very tired.
     //Update: I now HAVE to load them dynamically, so uh
-
-    class Person {
-        public string $id;
-        public string $fname;
-        public string $lname;
-        public string $position;
-        public string $imageLink;
-        public int $year;
-
-        public function __construct(string $id, string $fname, string $lname, string $position, string $imageLink, int $year) {
-            $this->id = $id;
-            $this->fname = $fname;
-            $this->lname = $lname;
-            $this->imageLink = $imageLink;
-            $this->position = $position;
-            $this->year = $year;
-        }
-
-        public function cardLoader(): void {
-            echo '<div class="col-12 col-sm-6 col-lg-3">
-                        <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
-                            <!-- Team Thumb-->
-                                <div class="advisor_thumb">
-                                    <!--anchor with link to detail, image b/t anchor tags-->
-                                    <a href="detail.php?id=', $this->id, '"><img src="', $this->imageLink, '" alt="" style = "width: 315px; height: 315px;"></a>
-                                    <!-- Social Info, I refuse to do facebook, no shot-->
-                                    <div class="social-info"><a href="detail.php?id=', $this->id, '"><i 
-                                    class="fa fa-facebook"></i></a><a href="detail.php?id=', $this->id, '"><i
-                                    class="fa fa-twitter"></i></a><a href="detail.php?id=', $this->id, '"><i 
-                                    class="fa fa-linkedin"></i></a></div>
-                                </div>
-                                <!-- Team Details-->
-                                <div class="single_advisor_details_info">
-                                    <h6>', $this->fname, ' ', $this->lname, '</h6>
-                                    <p class="designation">', $this->position, '</p>
-                                    <div>';
-            for ($i = 0; $i < $this->year; $i++) {
-                echo '<span class="h5 bi-slash-lg"></span>';
-            }
-            echo '</div>
-                                </div>
-                        </div>
-                  </div>';
-        }
-    }
-
-    $peeps = array(
-        new Person ("DG01", 'Dylan', 'Gaines', 'Web Developer', 'https://nku.instructure.com/images/thumbnails/4624798/cOaJDtQ1ZeEaNvU7y14k9yYOsh4PONg0hvzVuD9Q', 3),
-        new Person  ("ES01", 'Elise', 'Stromberg', 'Cyber-Security Specialist', 'https://nku.instructure.com/images/thumbnails/4370193/98G7lZ2ehtC90eVoURtFLRvHF2KMzTbujBiQLrE5', 3),
-        new Person  ("AL01", 'Alex', 'Lisa', 'Cyber-Security Specialist', 'https://nku.instructure.com/images/thumbnails/2771178/eXKo8itQM50KEetPXVKGMZ2uzsuFNk8tCyKcNKB8', 2),
-        new Person  ("QR01", 'Quentin', 'Roa', 'Web Developer', 'https://nku.instructure.com/images/thumbnails/3687491/xbUWCnAPRxLyisdFb4ADLlRGceYmL64DSkAEghvj', 2)
-    );
+    require_once("data.php");
+    require_once("functions.php");
     ?>
 
     <!-- https://www.bootdey.com/snippets/view/single-advisor-profile#html -->
@@ -86,6 +36,17 @@
             -webkit-transition-duration: 500ms;
             transition-duration: 500ms;
         }
+
+        .single_advisor_profile:hover ul {
+            -webkit-transition-duration: 500ms;
+            transition-duration: 500ms;
+            color: #FFFFFF;
+        }
+
+        .single_advisor_profile ul {
+            -webkit-transition-duration: 500ms;
+            transition-duration: 500ms;
+        }
     </style>
 
     <title>ASE 230 - class of Spring/Fall/Summer 20XX</title>
@@ -111,10 +72,7 @@
         <!-- Single Advisor-->
         <!--FIRST card-->
         <?php
-        $end = count($peeps);
-        for ($i = 0; $i <= $end - 1; $i++) {
-            $peeps[$i]->cardLoader();
-        }
+        cardLoader($studentArray);
         ?>
     </div>
 </div>
